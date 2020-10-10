31d1a0ee96b461beff4851db00e78f45e646a13d
Dávid Kecskeméti <dxdy421@gmail.com>
2020-10-10 11:53:19 +0200

    Initial commit

---
+++ src/main/java/com/dk/thymeleaf/thymeleaflayouts/ThymeleafLayoutsApplication.java
@@ -0,0 +1,13 @@
+package com.dk.thymeleaf.thymeleaflayouts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafLayoutsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafLayoutsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dk/thymeleaf/thymeleaflayouts/ThymeleafLayoutsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dk.thymeleaf.thymeleaflayouts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafLayoutsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
